f9b8ef1ea2359b3d3a0a4bf675852144a0cb88ba
arikawa <arikawa@i3-systems.com>
2020-04-25 01:53:49 +0900

    add spec 商品一覧を取得(items#index)

---
+++ ramen_shop_order_api/spec/api/features/list_item_spec.rb
@@ -0,0 +1,45 @@
+# spec/web/features/list_books_spec.rb
+
+RSpec.describe 'List items' do
+  include Rack::Test::Methods
+  let(:app) { Hanami.app }
+
+  let(:repository) { ItemRepository.new }
+  before do
+    repository.clear
+
+    repository.create(name: '塩ラーメン', price: 750)
+    repository.create(name: '豚骨ラーメン', price: 800)
+  end
+
+  context "GET /api/items" do
+    subject do
+      header 'Content-type', 'application/json'
+      get '/api/items'
+
+      last_response
+    end
+
+    let(:response_body) do
+      {
+        items:
+          [
+            {
+              name: '塩ラーメン',
+              price: 750
+            },
+            {
+              name: '豚骨ラーメン',
+              price: 800
+            }
+          ]
+      }
+    end
+
+    it 'is json response of items' do
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq response_body
+      expect(last_response.content_type).to include "application/json"
+    end
+  end
+end
