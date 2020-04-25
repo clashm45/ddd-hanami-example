e43b8147ef623c28629644c015ea332e3d0d0620
arikawa <arikawa@i3-systems.com>
2020-04-25 22:05:12 +0900

    spec 商品が空の場合

--- ramen_shop_order_api/spec/api/features/list_item_spec.rb
+++ ramen_shop_order_api/spec/api/features/list_item_spec.rb
@@ -3,41 +3,57 @@ RSpec.describe 'List items' do
   let(:app) { Hanami.app }
 
   let(:repository) { ItemRepository.new }
-  before do
-    repository.clear
-
-    repository.create(name: '塩ラーメン', price: 750)
-    repository.create(name: '豚骨ラーメン', price: 800)
-  end
-
+  
   context "GET /api/items" do
-    before do
+
+    let(:do_request) do
       header 'Content-type', 'application/json'
       get '/api/items'
-
-      last_response
     end
 
-    let(:response_body) do
-      {
-        items:
-          [
-            {
-              name: '塩ラーメン',
-              price: 750
-            },
-            {
-              name: '豚骨ラーメン',
-              price: 800
-            }
-          ]
-      }
+    context "商品が0件の場合" do
+
+      let(:response_body) { { items: [] } }
+
+      it 'is empty by default' do
+        do_request
+        expect(last_response.status).to eq 200
+        expect(last_response.content_type).to include "application/json"
+        expect(last_response.body).to eq response_body
+      end
     end
 
-    it 'is json response of items' do
-      expect(last_response.status).to eq 200
-      expect(last_response.content_type).to include "application/json"
-      expect(last_response.body).to eq response_body
+    context "商品が登録済みの場合" do
+      before do
+        repository.clear
+
+        repository.create(name: '塩ラーメン', price: 750)
+        repository.create(name: '豚骨ラーメン', price: 800)
+      end
+
+      let(:response_body) do
+        {
+          items:
+            [
+              {
+                name:  '塩ラーメン',
+                price: 750
+              },
+              {
+                name:  '豚骨ラーメン',
+                price: 800
+              }
+            ]
+        }
+      end
+
+      it 'is json response of items' do
+        do_request
+        expect(last_response.status).to eq 200
+        expect(last_response.content_type).to include "application/json"
+        expect(last_response.body).to eq response_body
+      end
+
     end
   end
 end
