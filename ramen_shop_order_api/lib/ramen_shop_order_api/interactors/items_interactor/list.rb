632ca07168ea5a8aca0b1208dfe8bbe8360342b4
arikawa <arikawa@i3-systems.com>
2020-04-26 15:27:52 +0900

    Item#List Interactorを実装

---
+++ ramen_shop_order_api/lib/ramen_shop_order_api/interactors/items_interactor/list.rb
@@ -0,0 +1,17 @@
+require 'hanami/interactor'
+
+module ItemsInteractor
+  class List
+    include Hanami::Interactor
+
+    expose :items
+
+    def initialize(repository: ItemRepository.new)
+      @repository = repository
+    end
+
+    def call
+      @items = @repository.all
+    end
+  end
+end
---
+++ ramen_shop_order_api/spec/ramen_shop_order_api/interactors/items_interactor/items_interactor_list_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+
+describe ItemsInteractor::List do
+  let(:interactor) { ItemsInteractor::List.new }
+  let(:result) { interactor.call }
+  let(:repository) { ItemRepository.new }
+
+  before do
+    repository.clear
+  end
+
+  context "商品が存在しない場合" do
+    it "succeeds" do
+      expect(result.successful?).to be(true)
+    end
+
+    it "空配列を返す" do
+      expect(result.items.length).to eq 0
+    end
+  end
+
+  context "商品が存在する場合" do
+    before do
+      @item = repository.create(name: 'Three Seven', price: 777)
+    end
+
+    it "succeeds" do
+      expect(result.successful?).to be(true)
+    end
+
+    it "list a Item with correct name and price" do
+      expect(result.items.length).to eq 1
+      item = result.items[0]
+      expect(item.name).to eq(@item.name)
+      expect(item.price).to eq(@item.price)
+    end
+  end
+end
