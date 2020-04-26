10e85c8e1ef724d13d8ca8995700997cc47621a0
arikawa <arikawa@i3-systems.com>
2020-04-26 13:41:11 +0900

    getting started - Interactor

---
+++ ramen_shop_order_api/lib/ramen_shop_order_api/interactors/books_interactor/create.rb
@@ -0,0 +1,17 @@
+require 'hanami/interactor'
+
+module BooksInteractor
+  class Create
+    include Hanami::Interactor
+
+    expose :book
+
+    def initialize(repository: BookRepository.new)
+      @repository = repository
+    end
+
+    def call(book_attributes)
+      @book = @repository.create(book_attributes)
+    end
+  end
+end
