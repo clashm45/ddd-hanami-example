7c73bb34edd605cc39154667bc19f60dd2b51d30
arikawa <arikawa@i3-systems.com>
2020-04-26 18:47:46 +0900

    schema.sql remove books
    おそらく hanami db apply を打ったあとにmigrateを復活させたのがまずかった。
    apply はmigarateファイルをschemaにマージして、migarateファイルを削除するコマンド。
    migrateファイルが大量になってきたときに使う想定らしい。

--- ramen_shop_order_api/db/schema.sql
+++ ramen_shop_order_api/db/schema.sql
@@ -1,2 +1,1 @@
 CREATE TABLE `schema_migrations` (`filename` varchar(255) NOT NULL PRIMARY KEY);
-CREATE TABLE `books` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `title` varchar(255) NOT NULL, `author` varchar(255) NOT NULL, `created_at` timestamp NOT NULL, `updated_at` timestamp NOT NULL);
