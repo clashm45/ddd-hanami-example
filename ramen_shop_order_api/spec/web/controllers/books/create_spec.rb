100d410bff897cf897a9a23496beb0296964536a
arikawa <arikawa@i3-systems.com>
2020-04-26 14:06:42 +0900

    Controllerでinteractorを使うように修正

--- ramen_shop_order_api/spec/web/controllers/books/create_spec.rb
+++ ramen_shop_order_api/spec/web/controllers/books/create_spec.rb
@@ -1,7 +1,8 @@
 # spec/web/controllers/books/create_spec.rb
 
 RSpec.describe Web::Controllers::Books::Create do
-  let(:action) { described_class.new }
+  let(:action) { described_class.new(interactor: interactor) }
+  let(:interactor) { instance_double('BooksInteractor::Create', call: nil) }
   let(:repository) { BookRepository.new }
 
   before do
@@ -11,12 +12,9 @@ RSpec.describe Web::Controllers::Books::Create do
   context 'with valid params' do
     let(:params) { Hash[book: { title: 'Confident Ruby', author: 'Avdi Grimm' }] }
 
-    it 'creates a new book' do
+    it 'calls interactor' do
+      expect(interactor).to receive(:call)
       action.call(params)
-      book = repository.last
-
-      expect(book.id).to_not be_nil
-      expect(book.title).to eq(params.dig(:book, :title))
     end
 
     it 'redirects the user to the books listing' do
@@ -25,10 +23,27 @@ RSpec.describe Web::Controllers::Books::Create do
       expect(response[0]).to eq(302)
       expect(response[1]['Location']).to eq('/books')
     end
+
+    context "実際にデータを登録する" do
+      let(:interactor) { BooksInteractor::Create.new }
+
+      it 'creates a new book' do
+        action.call(params)
+        book = repository.last
+
+        expect(book.id).to_not be_nil
+        expect(book.title).to eq(params.dig(:book, :title))
+      end
+    end
   end
 
   context 'with invalid params' do
     let(:params) { Hash[book: {}] }
+
+    it 'does not call interactor' do
+      expect(interactor).to_not receive(:call)
+      action.call(params)
+    end
 
     it 'returns HTTP client error' do
       response = action.call(params)
