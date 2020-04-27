87bbc7a94d1526f5d22c79455d5f3afb5c5773ae
clashm45 <clash.m45@gmail.com>
2020-04-28 01:53:55 +0900

    spec modify

--- ramen_shop_order_api/spec/api/controllers/order_slip/create_spec.rb
+++ ramen_shop_order_api/spec/api/controllers/order_slip/create_spec.rb
@@ -5,10 +5,14 @@ RSpec.describe Api::Controllers::OrderSlip::Create, type: :action do
   let(:interactor) { OrderSlipInteractor::Create.new }
   let(:params) { Hash[] }
 
+  let(:response) { action.call(params) }
+
   context 'with valid params' do
     it 'status 200' do
       response = action.call(params)
       expect(response[0]).to eq(200)
+      expect(response[1]['Content-Type']).to eq("application/json; charset=utf-8")
+      expect(action.order_slip).not_to be nil
     end
   end
 end
