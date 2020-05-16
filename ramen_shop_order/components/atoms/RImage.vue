7a2eb35730ad293a84343ce7716d6a4cfdb5b807
clashm45 <clash.m45@gmail.com>
2020-05-17 02:08:20 +0900

    Atom 画像を実装

---
+++ ramen_shop_order/components/atoms/RImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <img :src="src" :alt="alt" :width="width" :height="height"/>
+</template>
+
+<script lang="ts">
+
+  import { Vue, Component, Prop } from 'vue-property-decorator'
+
+  /**
+   * 基本的な画像タグ <img>
+   */
+  @Component({})
+  export default class RImage extends Vue {
+
+    @Prop({ type: String, required: true })
+    readonly src!: string
+
+    @Prop({ type: String, default: 'alt string', required: true })
+    readonly alt!: string
+
+    @Prop({ type: Number, default: 300 })
+    width?: number
+
+    @Prop({ type: Number, default: 200 })
+    height?: number
+  }
+</script>
+
+<style scoped>
+
+</style>
