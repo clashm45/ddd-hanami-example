7d3b427fb02f6314c86e02ff57fa8d5e5a24d47b
clashm45 <clash.m45@gmail.com>
2020-05-16 16:32:35 +0900

    [TOP] 注文するボタンを作成

--- ramen_shop_order/pages/index.vue
+++ ramen_shop_order/pages/index.vue
@@ -1,23 +1,9 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        ramen_shop_order
-      </h1>
-      <h2 class="subtitle">
-        My well-made Nuxt.js project
-      </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
+        <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          注文する
         </a>
       </div>
     </div>
@@ -36,11 +22,11 @@ export default Vue.extend({
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
+/* Sample `apply` at-rules with Tailwind CSS */
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
-*/
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -50,23 +36,20 @@ export default Vue.extend({
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
 }
+
 
 .links {
   padding-top: 15px;
